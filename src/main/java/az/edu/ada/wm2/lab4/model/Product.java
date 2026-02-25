650c05c61c2ac46d749883ee1e251784bf9fdd54
Ismayil <150678829+Ismayil338@users.noreply.github.com>
2026-02-25 13:39:02 +0400

    Initial commit

---
+++ src/main/java/az/edu/ada/wm2/lab4/model/Product.java
@@ -0,0 +1,43 @@
+package az.edu.ada.wm2.lab4.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class Product {
+    private UUID id;
+    private String productName;
+    private BigDecimal price;
+    private LocalDate expirationDate;
+    public Product(UUID id, String productName, BigDecimal price, LocalDate expirationDate) {
+        this.id = id;
+        this.productName = productName;
+        this.price = price;
+        this.expirationDate = expirationDate;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public LocalDate getExpirationDate() {
+        return expirationDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", productName='" + productName + '\'' +
+                ", price=" + price +
+                ", expirationDate=" + expirationDate +
+                '}';
+    }
+}
